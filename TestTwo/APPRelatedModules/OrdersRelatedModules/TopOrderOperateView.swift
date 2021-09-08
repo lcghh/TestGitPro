24e8504edc5b8aa6e2e923372607ea32b1c42903
lichao <lichao3@ysbang.cn>
2021-09-08 23:31:43 +0800

    处理头部

--- TestTwo/APPRelatedModules/OrdersRelatedModules/TopOrderOperateView.swift
+++ TestTwo/APPRelatedModules/OrdersRelatedModules/TopOrderOperateView.swift
@@ -32,11 +32,8 @@ class TopOrderOperateView: UIView {
             ConstraintMaker.edges.equalToSuperview()
         }
         containView.snp.makeConstraints { ConstraintMaker in
-            ConstraintMaker.edges.equalToSuperview()
-        }
-        bottomLine.snp.makeConstraints { ConstraintMaker in
-            ConstraintMaker.left.right.bottom.equalToSuperview()
-            ConstraintMaker.height.equalTo(1)
+            ConstraintMaker.left.top.bottom.equalTo(self)
+            ConstraintMaker.right.equalTo(self)
         }
         
     }
@@ -50,39 +47,82 @@ class TopOrderOperateView: UIView {
             
             let btn = UIButton(type: .custom)
             btn.setTitle(str, for: .normal)
-            btn.setTitleColor(.red, for: .normal)
+            btn.setTitleColor(.black, for: .normal)
+            btn.addTarget(self, action: #selector(didTapButton(btn:)), for: .touchUpInside)
             btn.titleLabel?.font = UIFont.systemFont(ofSize: 14)
-            btn.backgroundColor = .blue
+//            btn.backgroundColor = .blue
+            
+            let badgeLable = UILabel()
+            
+            badgeLable.text = " 22 "
+            badgeLable.textColor = .red
+            badgeLable.font = UIFont.systemFont(ofSize: 10)
+            badgeLable.layer.borderWidth = 1.0
+            badgeLable.layer.borderColor = UIColor.black.cgColor
+            badgeLable.layer.cornerRadius = 8.0
+            
+            badgeLable.tag = 1000
+            
+            btn.addSubview(badgeLable)
+            
+            
             btnArray.append(btn)
 
         }
         
-        
+        let reverSec:UIButton = btnArray[btnArray.count - 2]
         
         for btn in btnArray {
             containView.addSubview(btn)
-            if let lastBtn = lastView {
+            if lastView != nil {
                 btn.snp.makeConstraints { ConstraintMaker in
-                    ConstraintMaker.top.bottom.width.equalTo(lastBtn)
-                    ConstraintMaker.left.equalTo(lastBtn.snp.right)
+                    ConstraintMaker.top.bottom.width.equalTo(lastView!)
+                    ConstraintMaker.left.equalTo(lastView!.snp.right)
                 }
             }else{
                 btn.snp.makeConstraints { ConstraintMaker in
                     ConstraintMaker.top.bottom.equalTo(containView)
-                    ConstraintMaker.left.equalTo(containView)
-                    ConstraintMaker.width.equalTo(60)
+                    ConstraintMaker.left.equalTo(containView).offset(10)
+                }
+                bottomLine.snp.makeConstraints { ConstraintMaker in
+                    ConstraintMaker.bottom.equalTo(containView)
+                    ConstraintMaker.centerX.equalTo(btn)
+                    ConstraintMaker.width.equalTo(25)
+                    ConstraintMaker.height.equalTo(2)
                 }
             }
+            let badge = btn.viewWithTag(1000)
+            badge?.snp.makeConstraints({ ConstraintMaker in
+                btn.titleLabel?.sizeToFit()
+                let size = btn.titleLabel?.bounds.size
+                ConstraintMaker.centerX.equalTo(btn).offset(size!.width / 2)
+                ConstraintMaker.centerY.equalTo(btn).offset(-15)
+                ConstraintMaker.height.equalTo(16)
+            })
+            
             lastView = btn
         }
-//        lastView?.snp.updateConstraints({ ConstraintMaker in
-//            ConstraintMaker.right.equalTo(containView)
-//        })
+        lastView!.snp.remakeConstraints { ConstraintMaker in
+            ConstraintMaker.top.bottom.equalTo(containView)
+            ConstraintMaker.right.equalTo(containView).offset(0)
+            ConstraintMaker.left.equalTo(reverSec.snp.right)
+            ConstraintMaker.width.equalTo(reverSec.snp.width).multipliedBy(0.85)
+        }
     }
-
+    
+    @objc func didTapButton(btn:UIButton) -> Void {
+        bottomLine.snp.remakeConstraints { ConstraintMaker in
+            ConstraintMaker.bottom.equalTo(containView)
+            ConstraintMaker.centerX.equalTo(btn)
+            ConstraintMaker.width.equalTo(25)
+            ConstraintMaker.height.equalTo(2)
+        }
+    }
+    
+    
+    // MARK:lazy init
     lazy var scrollview:UIScrollView = {
         let scrollview = UIScrollView()
-        
         return scrollview
     }()
     
@@ -94,7 +134,7 @@ class TopOrderOperateView: UIView {
     
     lazy var containView:UIView = {
         let view = UIView()
-        
+        view.backgroundColor = .white
         return view
     }()
     
